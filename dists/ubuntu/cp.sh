3fb409a0d13e42c25b8edd0bca9c568371d94cbc
kingtiger01 <mnovick1988@gmail.com>
2014-08-14 17:05:55 -0400

    creating directorys

---
+++ dists/ubuntu/cp.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+cp -rvn /mnt/mirrors.rit.edu/dists/raring/main /mnt/mirrors.rit.edu/dists/raring/restricted /mnt/mirrors.rit.edu/dists/raring/universe /mnt/mirrors.rit.edu/dists/raring/multiverse ./
